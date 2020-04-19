f4b992e069eed28419dd06623b75827275d4c3cb
Asaf Saar <saarasaf@gmail.com>
2020-04-19 20:55:06 +0300

    Lesson 5 Files

---
+++ lesson5/segments.s
@@ -0,0 +1,36 @@
+.segment "CROWNLANDCHARSET"
+_L_CROWNLANDCHARSET: 
+.incbin "crownland_charset.bin"
+.export _L_CROWNLANDCHARSET
+
+.segment "CROWNLANDCHARSET2"
+_L_CROWNLANDCHARSET2: 
+.incbin "crownland_charset2.bin"
+.export _L_CROWNLANDCHARSET2
+
+.segment "CROWNLANDDLIST"
+_L_CROWNLANDDLIST: 
+.incbin "crownland_dlist.bin"
+.export _L_CROWNLANDDLIST
+
+.segment "CROWNLANDSCREEN"
+_L_CROWNLANDSCREEN: 
+.incbin "crownland_screen.bin"
+.export _L_CROWNLANDSCREEN
+
+.segment "CROWNLANDSCR1"
+_L_CROWNLANDSCR1: 
+.incbin "crownland_scr1.bin"
+.export _L_CROWNLANDSCR1
+
+
+.segment "CROWNLANDSCR2"
+_L_CROWNLANDSCR2: 
+.incbin "crownland_scr2.bin"
+.export _L_CROWNLANDSCR2
+
+.segment "CROWNLANDSCR3"
+_L_CROWNLANDSCR3: 
+.incbin "crownland_scr3.bin"
+.export _L_CROWNLANDSCR3
+
